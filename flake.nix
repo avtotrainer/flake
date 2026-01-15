ed0c55a154cba1009ce4e74a53a38a59e92c1315
avtotrainer <trainer@din.ge>
2026-01-15 10:51:26 +0400

    Fix Home Manager backup option placement (final)

--- flake.nix
+++ flake.nix
@@ -20,12 +20,15 @@
       modules = [
         ./hosts/laptop/default.nix
 
+        # Home Manager as NixOS module
         home-manager.nixosModules.home-manager
 
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
+          # IMPORTANT:
+          # Do NOT put backupFileExtension here
           home-manager.users.avto = import ./home/avto.nix;
         }
       ];
