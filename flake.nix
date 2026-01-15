488d0077c5e09ed6046f07d163f4fb52fee992ab
avtotrainer <trainer@din.ge>
2026-01-15 08:33:49 +0400

    Fix flake.nix: proper Home Manager integration

--- flake.nix
+++ flake.nix
@@ -3,24 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
-
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
   in {
     nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      inherit system pkgs;
+      inherit system;
+
+      # აქ გადაეცემა ყველაფერი module-ებს და Home Manager-ს
+      specialArgs = {
+        hostname = "nixos";
+      };
 
       modules = [
+        # შენი არსებული host კონფიგი
         ./hosts/laptop/default.nix
+
+        # Home Manager როგორც NixOS module
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.avto = import ./home/avto.nix;
+        }
       ];
     };
   };
 }
-
