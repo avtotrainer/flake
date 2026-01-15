872a89fb1f5b4f3cac01606a332ffb6fef1b1298
avtotrainer <trainer@din.ge>
2026-01-15 09:25:46 +0400

    Finalize zsh + powerlevel10k declarative setup

--- home/zsh/zsh.nix
+++ home/zsh/zsh.nix
@@ -8,9 +8,9 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    # oh-my-zsh მხოლოდ plugins-ებისთვის (არანაირი theme)
     oh-my-zsh = {
       enable = true;
-      theme = "powerlevel10k/powerlevel10k";
       plugins = [
         "git"
         "sudo"
@@ -18,13 +18,22 @@
       ];
     };
 
+    # Powerlevel10k — standalone, declarative
     initContent = ''
+      # Powerlevel10k theme
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+
+      # User-selected prompt config (from repo)
+      source ~/.p10k.zsh
     '';
   };
 
+  # Required packages
   home.packages = with pkgs; [
     zsh-powerlevel10k
     zoxide
   ];
+
+  # Ensure p10k config is owned by Home Manager
+  home.file.".p10k.zsh".source = ./p10k.zsh;
 }
