c923bb364249aadafb32f5e52665574556ff1834
avtotrainer <trainer@din.ge>
2026-01-15 10:13:55 +0400

    Fetch NvChad config via pkgs.fetchGit instead of submodule

--- home/config/config.nix
+++ home/config/config.nix
@@ -1,11 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   xdg.configFile."alacritty".source = ./alacritty;
   xdg.configFile."hypr".source       = ./hypr;
-  xdg.configFile."nvim".source       = ./nvim;
   xdg.configFile."waybar".source     = ./waybar;
   xdg.configFile."lsd".source        = ./lsd;
   xdg.configFile."vifm".source       = ./vifm;
   xdg.configFile."fontconfig".source = ./fontconfig;
+
+  # NvChad from GitHub (authoritative source)
+  xdg.configFile."nvim".source = pkgs.fetchGit {
+    url = "git@github.com:avtotrainer/nvchad-2.5-config.git";
+  };
 }
