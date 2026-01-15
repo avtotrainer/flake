aa0c4f12c21c199bfc149e57635976f2b9d00eb7
avtotrainer <trainer@din.ge>
2026-01-15 10:33:30 +0400

    Use networking.hostName in Home Manager instead of custom hostname arg

--- home/avto.nix
+++ home/avto.nix
@@ -1,55 +1,32 @@
-{ config, pkgs, hostname, ... }:
+{ config, pkgs, ... }:
 
 {
   home.username = "avto";
   home.homeDirectory = "/home/avto";
 
-  # -------------------------
-  # Session variables
-  # -------------------------
   home.sessionVariables = {
-    HOSTNAME_FROM_FLAKE = hostname;
+    HOSTNAME_FROM_FLAKE = config.networking.hostName;
     EDITOR = "nvim";
     VISUAL = "nvim";
+    NVIM_APPNAME = "nvim";
   };
 
-  # -------------------------
-  # Imports
-  # -------------------------
   imports = [
     ./zsh/zsh.nix
     ./config/config.nix
   ];
 
-  # -------------------------
-  # User packages
-  # -------------------------
   home.packages = with pkgs; [
     git
     neovim
-    vscode
     waybar
-
-    nodejs_20
-    go
-    gopls
-
-    (python312.withPackages (ps: [
-      ps.pip
-      ps.virtualenv
-      ps.setuptools
-      ps.wheel
-    ]))
   ];
 
-  # -------------------------
-  # Git
-  # -------------------------
   programs.git = {
     enable = true;
-    settings = {
-      user.name = "avto";
-      user.email = "avto@example.com";
+    settings.user = {
+      name = "avto";
+      email = "avto@example.com";
     };
   };
 
