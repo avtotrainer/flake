0f240bc5209f3a7868ae68176c61861b1114b9c0
avtotrainer <trainer@din.ge>
2026-01-15 10:46:30 +0400

    Allow Home Manager to backup existing ~/.config files

--- home/avto.nix
+++ home/avto.nix
@@ -4,6 +4,11 @@
   home.username = "avto";
   home.homeDirectory = "/home/avto";
 
+  # --- IMPORTANT ---
+  # Allow Home Manager to take over existing ~/.config files safely
+  home-manager.backupFileExtension = "bak";
+
+  # --- Session / Environment ---
   home.sessionVariables = {
     HOSTNAME_FROM_FLAKE = osConfig.networking.hostName;
     EDITOR = "nvim";
@@ -11,17 +16,20 @@
     NVIM_APPNAME = "nvim";
   };
 
+  # --- Imports ---
   imports = [
     ./zsh/zsh.nix
     ./config/config.nix
   ];
 
+  # --- User packages (UI + tools) ---
   home.packages = with pkgs; [
     git
     neovim
     waybar
   ];
 
+  # --- Git ---
   programs.git = {
     enable = true;
     settings.user = {
@@ -30,5 +38,6 @@
     };
   };
 
+  # --- REQUIRED ---
   home.stateVersion = "25.11";
 }
