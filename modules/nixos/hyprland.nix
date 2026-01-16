8c7f49c4ce23883e55189a2bc6b4ea8143b48a07
avtotrainer <trainer@din.ge>
2026-01-17 01:33:44 +0400

    Hyprland შესწორდა hyprland-ით

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   services.xserver.enable = false;
 
@@ -5,10 +7,16 @@
 
   services.greetd = {
     enable = true;
-    settings.default_session = {
-      command = "hyprland";
-      user = "avto";
+    settings = {
+      default_session = {
+        command = "tuigreet --cmd hyprland";
+        user = "avto";
+      };
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    tuigreet
+  ];
 }
 
