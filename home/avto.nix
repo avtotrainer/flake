a49187a7e20db2940151707c096f08e6d568fbc6
avtotrainer <trainer@din.ge>
2026-01-16 23:20:41 +0400

    Waybar სერვისის გადაკეთება

--- home/avto.nix
+++ home/avto.nix
@@ -1,34 +1,63 @@
 { pkgs, osConfig, ... }:
 
 {
+  ##################################################
+  # USER BASICS
+  ##################################################
   home.username = "avto";
   home.homeDirectory = "/home/avto";
 
+  ##################################################
+  # SESSION VARIABLES
+  ##################################################
   home.sessionVariables = {
     HOSTNAME_FROM_FLAKE = osConfig.networking.hostName;
+
     EDITOR = "nvim";
     VISUAL = "nvim";
     NVIM_APPNAME = "nvim";
   };
 
+  ##################################################
+  # IMPORTS
+  ##################################################
   imports = [
     ./zsh/zsh.nix
     ./config/config.nix
   ];
 
+  ##################################################
+  # PACKAGES (NO WAYBAR HERE)
+  ##################################################
   home.packages = with pkgs; [
     git
     neovim
-    waybar
   ];
 
+  ##################################################
+  # WAYBAR — CORRECT, DECLARATIVE WAY
+  ##################################################
+  programs.waybar = {
+    enable = true;
+
+    # თუ დაგჭირდება debug:
+    # systemd.enable = true;  # default = true
+  };
+
+  ##################################################
+  # GIT
+  ##################################################
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "avto";
-      email = "avto@example.com";
+    settings = {
+      user.name = "avto";
+      user.email = "avto@example.com";
     };
   };
 
+  ##################################################
+  # REQUIRED BY HOME-MANAGER
+  ##################################################
   home.stateVersion = "25.11";
 }
+
