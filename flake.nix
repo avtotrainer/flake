3eb7fb6261e48646d4ccba3bea264b1877354102
avtotrainer <trainer@din.ge>
2026-01-18 11:56:18 +0400

    flake.nix გასწორება

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "avto — NixOS Hyprland Laptop (flake-based)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -19,21 +19,16 @@
 
       modules = [
         ./hosts/laptop/default.nix
-
         home-manager.nixosModules.home-manager
 
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
-          # აუცილებელია conflicts-ისთვის
           home-manager.backupFileExtension = "bak";
 
-          # ⚠️ დროებითი "გადასვლის რეჟიმი":
-          # HM აღარ ეცდება old broken waybar.service-ის გაპარსვას/გადართვას
-          home-manager.users.avto = { ... }: {
+          home-manager.users.avto = {
             imports = [ ./home/avto.nix ];
-            # systemd.user.startServices = false;
           };
         }
       ];
