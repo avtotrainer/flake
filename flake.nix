f7e2e3e81cdbe66ddb654cde586a20a758c040b4
avtotrainer <trainer@din.ge>
2026-01-18 12:51:48 +0400

    4. flake.nix გასწორება

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-
-
+   
   home-manager = {
   url = "github:nix-community/home-manager/release-25.11";
   inputs.nixpkgs.follows = "nixpkgs";
+};
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
