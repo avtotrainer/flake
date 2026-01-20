51066709b38fd66b24c2b011c0e5c3ea48fafda8
avtotrainer <trainer@din.ge>
2026-01-20 22:09:47 +0400

    ვცდილობ Alacritty-ს განიქსებას

--- home/config/config.nix
+++ home/config/config.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 
 {
-  # xdg.configFile."alacritty".source = ./alacritty;
+  xdg.configFile."alacritty".source = ./alacritty;
   xdg.configFile."hypr".source       = ./hypr;
-  # xdg.configFile."waybar".source     = ./waybar;
+  xdg.configFile."waybar".source     = ./waybar;
   xdg.configFile."lsd".source        = ./lsd;
   xdg.configFile."vifm".source       = ./vifm;
 
