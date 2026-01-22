f571fdf92a5fd163e86198b05a08c5f8d0a2662e
avtotrainer <trainer@din.ge>
2026-01-22 18:04:35 +0400

    Fix users/avto.nix syntax after user split

--- users/avto.nix
+++ users/avto.nix
@@ -1,18 +1,10 @@
 { pkgs, ... }:
 
 {
-  users.users.avto = {
-    isNormalUser = true;
-    uid 
-{ pkgs, ... }:
-
-{
   programs.zsh.enable = true;
 
   programs.zsh.interactiveShellInit = ''
     eval "$(zoxide init zsh --cmd z)"
   '';
 }
-
-
 
