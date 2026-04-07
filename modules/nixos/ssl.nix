13372fbc645fea414cf483fb6154c95a2de0c062
avtotrainer <trainer@din.ge>
2026-04-07 15:23:10 +0400

    nixos: trust school root CA

--- modules/nixos/ssl.nix
+++ modules/nixos/ssl.nix
@@ -1,16 +1,13 @@
 { ... }:
 
 {
-  environment.variables = {
-    SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
-    NIX_SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
-  };
-
-  nix.settings = {
-    ssl-cert-file = "/etc/ssl/certs/ca-certificates.crt";
-  };
-
   security.pki.certificateFiles = [
     ./certs/School_wifi.cer
   ];
+
+  environment.variables = {
+    SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
+    NIX_SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
+    CURL_CA_BUNDLE = "/etc/ssl/certs/ca-certificates.crt";
+  };
 }
