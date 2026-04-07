3d9c120d272ef099ac4a1f3bf2d0f1a937d150de
avtotrainer <trainer@din.ge>
2026-04-07 15:02:45 +0400

    nixos: trust school root CA

--- modules/nixos/ssl.nix
+++ modules/nixos/ssl.nix
@@ -1,14 +1,16 @@
-{  config, pkgs, ... }:
+{ ... }:
 
 {
   environment.variables = {
     SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
-    
     NIX_SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
-    
   };
 
   nix.settings = {
-    ssl-cert-file = "/etc/ssl/certs/ca-certificates.crt";    
+    ssl-cert-file = "/etc/ssl/certs/ca-certificates.crt";
   };
+
+  security.pki.certificateFiles = [
+    ./certs/School_wifi.cer
+  ];
 }
